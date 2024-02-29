233712ff3abff88a3157202037670466ce6d8408
YohanL5 <pachecoyohan99@gmail.com>
2024-02-29 08:40:17 -0500

    Se agrego productoDto y se modifico el Product Entity con las anotaciones de Spring

--- Restaurant/src/main/java/com/restaurant/Restaurant/entity/ProductEntity.java
+++ Restaurant/src/main/java/com/restaurant/Restaurant/entity/ProductEntity.java
@@ -1,16 +1,32 @@
 package com.restaurant.Restaurant.entity;
+import jakarta.persistence.*;
+
 import java.util.*;
 
+@Entity
+@Table
 public class ProductEntity {
 
     public enum Category{
         HAMBURGERS_AND_HOTDOGS, CHICKEN, FISH, MEATS, DESSERTS, VEGAN_FOOD, KIDS_MEALS
     }
 
+    @Id
+    @Column(name = "product_uuid", nullable = false)
     private UUID uuid;
+
+    @Column(name = "name", nullable = false)
     private String fantasyName;
+
+    @Column(name = "category", nullable = false)
     private Category category;
+
+    @Column(name = "description", nullable = false)
     private String description;
+
+    @Column(name = "price", nullable = false)
     private double price;
+
+    @Column(name = "available", nullable = false)
     private boolean available;
 }
---
+++ Restaurant/src/main/java/com/restaurant/Restaurant/models/dto/ProductDTO.java
@@ -0,0 +1,15 @@
+package com.restaurant.Restaurant.models.dto;
+
+import com.restaurant.Restaurant.entity.ProductEntity;
+import lombok.Data;
+import java.util.UUID;
+
+@Data
+public class ProductDTO {
+    private UUID uuid;
+    private String fantasyName;
+    private ProductEntity.Category category;
+    private String description;
+    private double price;
+    private boolean available;
+}
