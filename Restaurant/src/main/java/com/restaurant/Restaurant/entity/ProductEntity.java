a1ba4e1e0808d2881aa107158ad319a17e5cc297
YohanL5 <pachecoyohan99@gmail.com>
2024-02-29 08:40:17 -0500

    Se agregó ProductRepository y se modificó ProductEntity con lo visto en el training

--- Restaurant/src/main/java/com/restaurant/Restaurant/entity/ProductEntity.java
+++ Restaurant/src/main/java/com/restaurant/Restaurant/entity/ProductEntity.java
@@ -1,24 +1,32 @@
 package com.restaurant.Restaurant.entity;
-import lombok.*;
+import jakarta.persistence.*;
 
 import java.util.*;
 
-@Builder
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Entity
+@Table
 public class ProductEntity {
 
     public enum Category{
         HAMBURGERS_AND_HOTDOGS, CHICKEN, FISH, MEATS, DESSERTS, VEGAN_FOOD, KIDS_MEALS
     }
 
-    private Long id;
+    @Id
+    @Column(name = "product_uuid", nullable = false)
     private UUID uuid;
+
+    @Column(name = "name", nullable = false)
     private String fantasyName;
+
+    @Column(name = "category", nullable = false)
     private Category category;
+
+    @Column(name = "description", nullable = false)
     private String description;
+
+    @Column(name = "price", nullable = false)
     private double price;
+
+    @Column(name = "available", nullable = false)
     private boolean available;
 }
