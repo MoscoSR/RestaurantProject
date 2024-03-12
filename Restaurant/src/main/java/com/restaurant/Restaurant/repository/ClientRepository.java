cfa3c3f0e328dcb25657cb054c6b0b291b636099
AndresLegro <andreslegrog@gmail.com>
2024-03-11 20:20:19 -0500

    Se añade la funcionalidad de crear y actualizar a entregada una orden

--- Restaurant/src/main/java/com/restaurant/Restaurant/repository/ClientRepository.java
+++ Restaurant/src/main/java/com/restaurant/Restaurant/repository/ClientRepository.java
@@ -5,5 +5,5 @@ import com.restaurant.Restaurant.entity.ClientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<ClientEntity, Long> {
-
+    ClientEntity findByDocument(String document);
 }
--- Restaurant/src/main/java/com/restaurant/Restaurant/repository/IOrderRepository.java
+++ Restaurant/src/main/java/com/restaurant/Restaurant/repository/IOrderRepository.java
@@ -1,8 +1,10 @@
 package com.restaurant.Restaurant.repository;
 
 import com.restaurant.Restaurant.entity.OrderEntity;
+import com.restaurant.Restaurant.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IOrderRepository {
+public interface IOrderRepository extends JpaRepository<OrderEntity, Long> {
 
     OrderEntity findByUuid(String uuid);
 
