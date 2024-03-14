87bbe799981da35134de2c3e486d9e6de3b3cb7f
JuanCarlos-A <juancar2805@gmail.com>
2024-03-14 12:48:05 -0500

    Order

--- Restaurant/src/main/java/com/restaurant/Restaurant/service/clients/ClientService.java
+++ Restaurant/src/main/java/com/restaurant/Restaurant/service/clients/ClientService.java
@@ -50,31 +50,13 @@ public class ClientService  implements IClientService {
     public ClientDTO createClient(ClientDTO clientDTO) {
         clientValidator.validateCliente(clientDTO);
         clientValidator.validateDocumentFormat(clientDTO.getDocument());
-<<<<<<< HEAD
-
-        if(clientRepository.existsByDocument(clientDTO.getDocument())){
-            throw new DocumentExistsException("El cliente ya existe");
-        }
-        ClientEntity clientEntity = new ClientEntity();
-        clientEntity.setName(clientDTO.getName());
-        clientEntity.setDocument(clientDTO.getDocument());
-        clientEntity.setEmail(clientDTO.getEmail());
-        clientEntity.setPhone(clientDTO.getPhone());
-        clientEntity.setDeliveryAddress(clientDTO.getDeliveryAddress());
-=======
         ClientEntity clientEntity = mapper.convertDTO(clientDTO);
->>>>>>> 4c99e94bcac515d3934a0d26f38cd89284736e82
         clientRepository.save(clientEntity);
         return mapper.convert(clientEntity);
     }
 
-<<<<<<< HEAD
     @Override
-    public void updateClient(String document, ClientDTO clientDTO) {
-
-=======
-    public  ClientDTO updateClient(String document, ClientDTO clientDTO) {
->>>>>>> 4c99e94bcac515d3934a0d26f38cd89284736e82
+    public ClientDTO updateClient(String document, ClientDTO clientDTO) {
         clientValidator.validateCliente(clientDTO);
         clientValidator.validateDocumentFormat(clientDTO.getDocument());
         ClientEntity clientEntity = clientRepository.findByDocument(document);
@@ -83,18 +65,6 @@ public class ClientService  implements IClientService {
             throw new ClientNotFoundException("No se encontró ningún cliente con el documento proporcionado");
         }
 
-<<<<<<< HEAD
-        try {
-            clientEntity.setName(clientDTO.getName());
-            clientEntity.setDocument(clientDTO.getDocument());
-            clientEntity.setEmail(clientDTO.getEmail());
-            clientEntity.setPhone(clientDTO.getPhone());
-            clientEntity.setDeliveryAddress(clientDTO.getDeliveryAddress());
-            clientRepository.save(clientEntity);
-            mapper.convert(clientEntity);
-        } catch (Exception e) {
-            throw new InternalServerError("Error general del servidor al actualizar el cliente");
-=======
         // Verificar si existe otro cliente con el mismo documento diferente al que se está editando
         ClientEntity existingClient = clientRepository.findByDocument(document);
 
@@ -103,7 +73,6 @@ public class ClientService  implements IClientService {
 
         if (clientRepository.existsByDocument(clientDTO.getDocument()) && clientValidator.clientExistByDocument(existingClient.getDocument(), clientDTO.getDocument())){
             throw new ClientNotFoundException("Cliente con" + clientDTO.getDocument() + "no existe");
->>>>>>> 4c99e94bcac515d3934a0d26f38cd89284736e82
         }
         //clientEntity.setId(clientDTO.getId());
         clientEntity.setName(clientDTO.getName());
